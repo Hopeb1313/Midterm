1b56a8fd4f475819068b920821f4323a78a43d60
Hope Beasley <hopeb1313@gmail.com>
2023-03-26 19:32:31 -0700

    commit and push

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@ $host = "en1ehf30yom7txe7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $user = "vp8744gaikmgekne"; 
 $pass = "hwntszmccbzzh0vx"; 
 $db = "gsx4xjtkbbf7dvh7"; 
-
+ 
 $conn = mysqli_connect($host, $user, $pass, $db);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
